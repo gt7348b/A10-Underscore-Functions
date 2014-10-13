2bf49bbc11129f33154c679054bd4c42445fac3f
John Crocker <johnniecrocker@yahoo.com>
2014-10-13 15:30:52 -0400

    set up namespace classes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,46 @@
 console.log('The Iron Yard Rocks');
+
+
+var test_array = [1, 2, 3, 4, 5, 6],
+    filter_object,
+    filter_return,
+    map_object,
+    map_function,
+    map_return,
+    replace_return;
+
+
+var roscoe = roscoe || {};
+
+roscoe.stuff = {
+
+
+//This is the filter
+
+    filter: function(name) {
+
+        console.log(name);
+
+    },
+
+
+//This is the map function
+
+    map: function(test_array) {
+
+
+    },
+
+
+//This is the reduce function
+
+    reduce: function(test_array) {
+    },
+
+
+//This is the other function
+
+    other: function(test_array) {
+    }
+
+};
